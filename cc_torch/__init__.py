099c635b217b478c3ed891b039beedcb87a51993
Ronghang Hu <ronghang.hu@gmail.com>
2024-07-17 23:19:37 +0000

    fix a circular import error

--- cc_torch/__init__.py
+++ cc_torch/__init__.py
@@ -1,3 +1,4 @@
+# pyre-ignore-all-errors
 from .connected_components import get_connected_components
 
 __all__ = ["get_connected_components"]
--- cc_torch/connected_components.py
+++ cc_torch/connected_components.py
@@ -1,5 +1,12 @@
-from cc_torch import _C
+# pyre-ignore-all-errors
+from typing import Tuple
+
+import torch
 
 
-def get_connected_components(x, get_counts=True):
-    return _C.cc_2d(x.contiguous(), get_counts)
+def get_connected_components(
+    x: torch.Tensor, get_counts: bool = True
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    from . import _C  # pyre-ignore[21]
+
+    return _C.cc_2d(x.contiguous(), get_counts)  # pyre-ignore[16]
